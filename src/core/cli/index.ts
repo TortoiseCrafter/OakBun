a2cbc2c7931e7ce072b2f4446fc54c3698377216
CodingRuo <r.schober@outlook.com>
2025-12-07 12:09:56 +0100

    Bugfix Command Import

--- src/core/cli/index.ts
+++ src/core/cli/index.ts
@@ -1,23 +1,21 @@
 import { Glob } from "bun"
 import consola from "consola"
 import path from 'node:path'
-import '../commands/db.commands'
 
 const BootConsole = async (dir?: string) => {
     await loadCommandsFrom(dir ?? './src/commands')
 }
 
 const loadCommandsFrom = async (dir: string): Promise<void> => {
-    const glob = new Glob('**/.*ts')
-    const absolutDir = path.join(process.cwd(), dir)
+    try {
+        const glob = new Glob('**/.*ts')
+        const absolutDir = path.join(process.cwd(), dir)
 
-    if (!absolutDir) {
+        for await (const file of glob.scan({ cwd: absolutDir })) {
+            await import(path.join(absolutDir, file))
+        }
+    } catch (error) {
         consola.warn('Keine Custom Commands')
-        return
-    }
-
-    for await (const file of glob.scan({ cwd: absolutDir })) {
-        await import(path.join(absolutDir, file))
     }
 }
 
